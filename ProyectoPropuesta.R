d494f327c431f991e4861116e422315df1529855
MiroslavaMontiel <miroslavamontieldc@gmail.com>
2022-12-11 13:53:21 -0600

    Plotlys finales
    Gráficos plotly de pastel finales

--- ProyectoPropuesta.R
+++ ProyectoPropuesta.R
@@ -12,9 +12,20 @@ library(readxl)
 library(readr)
 library(trelliscopejs)
 library(forecast)
+library(plotly)
+
+
+####Importar base de datos csv
+file.choose()#buscar ruta del archivo csv
+ruta_xlsx <-"C:\\Users\\Miroslava\\Documents\\IRC\\Proyecto Prototípico\\PP\\TB_SEC_XII.xlsx"
+TB_SEC_XII<-read_excel(ruta_xlsx)
+
+
+
+
 #Base de datos INEGI Número 12
 TB_SEC_XII %>%
-  filter(NOM_ENT == "CIUDAD DE MÉXICO", P12_4 != "3") %>%
+  filter(NOM_ENT == "CIUDAD DE MÉXICO", P12_6 != "3") %>%
   select(NOM_MUN, P12_4) %>%
   group_by(NOM_MUN) %>% #Ordena de manera ascendente, si quisiéramos desc tenemos que añadir desc
   summarise(count = n() )  %>%
@@ -22,15 +33,27 @@ TB_SEC_XII %>%
   mutate(parents="Casos totales de violencia en la niñez por")  %>%
   ungroup()
 
+cdmx<-TB_SEC_XII %>%
+  filter(NOM_ENT == "CIUDAD DE MÉXICO")
+
+TotEntrevistadas<-count(cdmx, "NOM_ENT")
+TotEntrevistadas
+
+
+
+#Personas que han sufritdo violencia en su niñez por alcaldía
+#totales
 V_n<-TB_SEC_XII %>%
-  filter(NOM_ENT == "CIUDAD DE MÉXICO", P12_4 != "3") %>%
+  filter(NOM_ENT == "CIUDAD DE MÉXICO", P12_6 != "3") %>%
   select(NOM_MUN, P12_4) %>%
   group_by(NOM_MUN) %>% #Ordena de manera ascendente, si quisiéramos desc tenemos que añadir desc
   summarise(count = n() )  %>%
   arrange(count)  %>%
-  mutate(parents="Casos totales donde la entrevistada presenció golpes en su casa durante su niñez")  %>%
+  mutate(parents="Casos totales donde la entrevistada sufrió golpes en su casa durante su niñez")  %>%
   ungroup()
 V_n
+
+#mapa de arbol totales 
 plot_ly(data =V_n,
         type="treemap",
         values = ~count,
@@ -39,19 +62,54 @@ plot_ly(data =V_n,
         domain= list(colum=0),
         name="Casos totales de violencia en la niñez por alcaldía",
         textinfo= "label+value+ parent"
-        )
+)
 
+#Personas que han sufritdo violencia en su niñez por alcaldía
+#Porcentaje respecto al total de entrevistadas que son 3267 personas
+V_n1<-V_n %>%
+  select(NOM_MUN, count) %>% 
+  mutate(Porcentaje = count*100/3267) %>% 
+  arrange(Porcentaje)  
+V_n1
 
+# 
+# P12_12 Numérico A1, A2, Cuando usted se enoja  o enojaba con sus hijas e hijos, ¿los insulta o
+# insultaba…
+# 1 - de vez en cuando?  2 - seguido 3 - No les insulta # 4 - No tiene hijos(as)
+# P12_7 Numérico  ¿Recuerda si las  personas con las que  vivía la insultaban o la  ofendían a usted…
+# 1 - de vez en cuando?  2 - seguido? 3 - No la insultaban ni la ofendían
 #Mujeres que si insultan a sus hijos, tienen antecedentes de insultos hacía su persona en la niñez? no, 53, si 47
 V_n<-TB_SEC_XII %>%
-  filter(NOM_ENT == "CIUDAD DE MÉXICO",NOM_MUN=="GUSTAVO A. MADERO",P12_12!= "3", P12_12!= "4" ) %>%
-  group_by(NOM_MUN) %>% #Ordena de manera ascendente, si quisiéramos desc tenemos que añadir desc
+  filter(NOM_ENT == "CIUDAD DE MÉXICO",P12_12!= "3", P12_12!= "4" ) %>%
+  group_by(NOM_ENT) %>% 
+  mutate(P12_7=case_when(P12_7=="1"~"si",P12_7=="2"~"si", P12_7=="3"~"no")) %>%
   count(P12_7) %>% 
-  mutate(porcentaje=scales::percent(n/sum(n)))  %>%
-  ggplot(aes(x=NOM_MUN, y=n, fill=P12_7), position="dodge")+
-  geom_bar(stat="identity")+
-  geom_text(aes(label=porcentaje), color="white", vjust=0.001, position=position_dodge (2))
+  mutate(porcentaje=scales::percent(n/sum(n)))  
 V_n
+
+# GGPLOT
+g1<-ggplot(V_n,aes(x="", y=porcentaje, fill=P12_7))+
+  geom_col(color = "black") +
+  geom_label(aes(label = porcentaje), color =  "white",
+             position = position_stack(vjust = 0.5),
+             show.legend = FALSE) +
+  guides(fill = guide_legend(title = "respuesta")) +
+  scale_fill_brewer(palette = "Accent") +
+  coord_polar(theta = "y") + 
+  theme_void()
+
+g1
+
+# *****************************************Plotly 1 INICIO
+
+p1 <- plot_ly(data = V_n, labels = ~P12_7, values = ~n, type = 'pie',textinfo = "label+percent",
+              insidetextorientation = "radial")  %>% layout(title = 'Mujeres que insultan a sus hijos, ¿sufrieron insultos en su niñez?')
+
+p1
+
+
+# *****************************************Plotly 2 FIN
+
 
 #Mujeres que no insultan a sus hijos, tienen antecedentes de insultos hacía su persona en la niñez? no, 84.8 si 15.2
 V_n<-TB_SEC_XII %>%
@@ -64,6 +122,48 @@ V_n<-TB_SEC_XII %>%
   geom_text(aes(label=porcentaje), color="white", vjust=0.001, position=position_dodge (2))
 V_n
 
+
+
+V_n2<-TB_SEC_XII %>%
+  filter(NOM_ENT == "CIUDAD DE MÉXICO",P12_12!= "1", P12_12!= "2" ) %>%
+  group_by(NOM_ENT) %>% 
+  mutate(P12_7=case_when(P12_7=="1"~"si",P12_7=="2"~"si", P12_7=="3"~"no")) %>%
+  count(P12_7) %>% 
+  mutate(porcentaje=scales::percent(n/sum(n)))  
+V_n2
+
+#GGPLOT
+g2<-ggplot(V_n2,aes(x="", y=porcentaje, fill=P12_7))+
+  geom_col(color = "black") +
+  geom_label(aes(label = porcentaje), color =  "white",
+             position = position_stack(vjust = 0.5),
+             show.legend = FALSE) +
+  guides(fill = guide_legend(title = "respuesta")) +
+  scale_fill_brewer(palette = "Accent") +
+  coord_polar(theta = "y") + 
+  theme_void()
+
+g2
+
+# *****************************************Plotly 2 INICIO
+
+p2 <- plot_ly(data = V_n2, labels = ~P12_7, values = ~n, type = 'pie',textinfo = "label+percent",
+              insidetextorientation = "radial")  %>% layout(title = 'Mujeres que NO INSULTAN a sus hijos, ¿sufrieron insultos en su niñez?')
+
+p2
+
+
+# *****************************************Plotly 2 FIN
+
+
+
+
+# P12_13  Cuando usted se enoja o enojaba con sus hijas e hijos, ¿les pega o pegaba…
+# # 1 - de vez en cuando?  2 - seguido?  3 - No les pega 4 - No tiene hijos(as)
+# P12_6 Numérico A1, A2, ¿Las personas con las que vivía le pegaban a usted…
+# 1 - de vez en cuando? 2 - seguido? 3 - No le pegaban
+
+
 #Mujeres que golpean a sus hijos, tienen antecedentes de golpes hacía su persona en la niñez? no, 43.4 si 56.7
 V_n<-TB_SEC_XII %>%
   filter(NOM_ENT == "CIUDAD DE MÉXICO",NOM_MUN=="GUSTAVO A. MADERO",P12_13!= "3",P12_13!= "4") %>%
@@ -74,6 +174,41 @@ V_n<-TB_SEC_XII %>%
   geom_bar(stat="identity")+
   geom_text(aes(label=porcentaje), color="white", vjust=0.001, position=position_dodge (2))
 V_n
+
+V_n3<-TB_SEC_XII %>%
+  filter(NOM_ENT == "CIUDAD DE MÉXICO",P12_13!= "3",P12_13!= "4") %>%
+  group_by(NOM_ENT) %>% 
+  mutate(P12_6=case_when(P12_6=="1"~"si",P12_6=="2"~"si", P12_6=="3"~"no")) %>%
+  count(P12_6) %>% 
+  mutate(porcentaje=scales::percent(n/sum(n)))  
+V_n3
+
+#GGPLOT 3
+g3<-ggplot(V_n3,aes(x="", y=porcentaje, fill=P12_6))+
+  geom_col(color = "black") +
+  geom_label(aes(label = porcentaje), color =  "white",
+             position = position_stack(vjust = 0.5),
+             show.legend = FALSE) +
+  guides(fill = guide_legend(title = "respuesta")) +
+  scale_fill_brewer(palette = "Accent") +
+  coord_polar(theta = "y") + 
+  theme_void()
+
+g3
+
+# *****************************************Plotly 3 INICIO
+
+p3 <- plot_ly(data = V_n3, labels = ~P12_6, values = ~n, type = 'pie',textinfo = "label+percent",
+              insidetextorientation = "radial")  %>% layout(title = 'Mujeres que SI GOLPEAN a sus hijos, ¿sufrieron insultos en su niñez?')
+
+p3
+
+
+# *****************************************Plotly 3 FIN
+
+
+
+
 
 #Mujeres no que golpean a sus hijos, tienen antecedentes de golpes hacía su persona en la niñez? no, 74 si 26
 V_n<-TB_SEC_XII %>%
@@ -86,6 +221,47 @@ V_n<-TB_SEC_XII %>%
   geom_text(aes(label=porcentaje), color="white", vjust=0.001, position=position_dodge (2))
 V_n
 
+V_n4<-TB_SEC_XII %>%
+  filter(NOM_ENT == "CIUDAD DE MÉXICO",P12_13!= "1",P12_13!= "2",P12_13!= "4") %>%
+  group_by(NOM_ENT) %>% 
+  mutate(P12_6=case_when(P12_6=="1"~"si",P12_6=="2"~"si", P12_6=="3"~"no")) %>%
+  count(P12_6) %>% 
+  mutate(porcentaje=scales::percent(n/sum(n)))  
+V_n4
+
+#GGPLOT 3
+g4<-ggplot(V_n4,aes(x="", y=porcentaje, fill=P12_6))+
+  geom_col(color = "black") +
+  geom_label(aes(label = porcentaje), color =  "white",
+             position = position_stack(vjust = 0.5),
+             show.legend = FALSE) +
+  guides(fill = guide_legend(title = "respuesta")) +
+  scale_fill_brewer(palette = "Accent") +
+  coord_polar(theta = "y") + 
+  theme_void()
+
+g4
+
+# *****************************************Plotly 4 INICIO
+
+p4 <- plot_ly(data = V_n4, labels = ~P12_6, values = ~n, type = 'pie',textinfo = "label+percent",
+              insidetextorientation = "radial")  %>% layout(title = 'Mujeres que SI GOLPEAN a sus hijos, ¿sufrieron insultos en su niñez?')
+
+p4
+# *****************************************Plotly 4 FIN
+
+
+
+
+
+
+# P12_8 Cuando su pareja o esposo era niño (hasta antes de cumplir 15 años), ¿le pegaban o
+# insultaban en su casa… 1 - de vez en cuando? 2 - seguido? 3 - No le pegaban ni lo insultaban 8 - No sabe b - blanco
+# P12_10Cuando su esposo o pareja se enoja o enojaba con sus hijas e hijos, ¿los
+# insulta o insultaba… 1 - de vez en cuando? 2 - seguido? 3 - No los insulta 4 - No tiene hijos(as) b - blanco
+
+
+
 
 #Parejas de mujeres que golpean o insultan a sus hijos, tienen antecedentes de golpes o insultos hacía su persona en la niñez? no, 42.9 si 57.1
 V_n<-TB_SEC_XII %>%
@@ -97,6 +273,38 @@ V_n<-TB_SEC_XII %>%
   geom_bar(stat="identity")+
   geom_text(aes(label=porcentaje), color="white", vjust=0.001, position=position_dodge (2))
 V_n
+
+V_n5<-TB_SEC_XII %>%
+  filter(NOM_ENT == "CIUDAD DE MÉXICO",P12_8!= "8",P12_8!= "b",P12_10!= "3",P12_10!= "4", P12_10!= "b") %>%
+  group_by(NOM_ENT) %>% 
+  mutate(P12_8=case_when(P12_8=="1"~"si",P12_8=="2"~"si", P12_8=="3"~"no")) %>%
+  count(P12_8) %>% 
+  mutate(porcentaje=scales::percent(n/sum(n)))  
+V_n5
+
+#GGPLOT 3
+g5<-ggplot(V_n5,aes(x="", y=porcentaje, fill=P12_8))+
+  geom_col(color = "black") +
+  geom_label(aes(label = porcentaje), color =  "white",
+             position = position_stack(vjust = 0.5),
+             show.legend = FALSE) +
+  guides(fill = guide_legend(title = "respuesta")) +
+  scale_fill_brewer(palette = "Accent") +
+  coord_polar(theta = "y") + 
+  theme_void()
+
+g5
+
+# *****************************************Plotly 4 INICIO
+
+p5 <- plot_ly(data = V_n5, labels = ~P12_8, values = ~n, type = 'pie',textinfo = "label+percent",
+              insidetextorientation = "radial")  %>% layout(title = 'Parejas de Mujeres que SI GOLPEAN a sus hijos, ¿sufrieron insultos en su niñez?')
+
+p5
+
+
+
+
 
 
 #Parejas de mujeres que NO golpean o insultan a sus hijos, tienen antecedentes de golpes o insultos hacía su persona en la niñez? no, 73 si 28
@@ -111,8 +319,35 @@ V_n<-TB_SEC_XII %>%
   geom_text(aes(label=porcentaje), color="white", vjust=0.001, position=position_dodge (2))
 V_n
 
+V_n6<-TB_SEC_XII %>%
+  filter(NOM_ENT == "CIUDAD DE MÉXICO",P12_8!= "8",P12_8!= "b",P12_10!= "1",P12_10!= "2",P12_10!= "4", P12_10!= "b") %>%
+  group_by(NOM_ENT) %>% 
+  mutate(P12_8=case_when(P12_8=="1"~"si",P12_8=="2"~"si", P12_8=="3"~"no")) %>%
+  count(P12_8) %>% 
+  mutate(porcentaje=scales::percent(n/sum(n)))  
+V_n6
 
+#GGPLOT 3
+g6<-ggplot(V_n6,aes(x="", y=porcentaje, fill=P12_8))+
+  geom_col(color = "black") +
+  geom_label(aes(label = porcentaje), color =  "white",
+             position = position_stack(vjust = 0.5),
+             show.legend = FALSE) +
+  guides(fill = guide_legend(title = "respuesta")) +
+  scale_fill_brewer(palette = "Accent") +
+  coord_polar(theta = "y") + 
+  theme_void()
 
+g6
+
+# *****************************************Plotly 4 INICIO
+
+p6 <- plot_ly(data = V_n6, labels = ~P12_8, values = ~n, type = 'pie',textinfo = "label+percent",
+              insidetextorientation = "radial")  %>% layout(title = 'Parejas de Mujeres que NO GOLPEAN a sus hijos, ¿sufrieron insultos en su niñez?')
+
+p6
+
+#GIFT
 #Base de datos línea Mujeres
 head(servicios_integrales_2019_2021)
 
@@ -218,78 +453,3 @@ max(predicciones[["upper"]])
 p_predict<-autoplot(predicciones)
 p_predict
 
-#CORRELACIONES
-names(TB_SEC_XII)
-attach(TB_SEC_XII)
-str(P12_13)
-str(P12_6)
-plot(P12_13,P12_6)
-cor.test(P12_13,P12_6)
-
-names(TB_SEC_XII)
-attach(TB_SEC_XII)
-str(P12_12)
-str(P12_7)
-plot(P12_12,P12_7)
-cor.test(P12_12,P12_7)
-
-
-
-#limpiando la base de datos
-#Insultos a sus hijos, tienen antecedentes de insultos hacía su persona en la niñez? no, 84.8 si 15.2
-V_n <- TB_SEC_XII[!(TB_SEC_XII$P12_12 == "4"),]
-
-V_n2<-V_n %>%
-  filter(NOM_ENT == "CIUDAD DE MÉXICO",NOM_MUN=="GUSTAVO A. MADERO") %>%
-  group_by(NOM_MUN) %>% #Ordena de manera ascendente, si quisiéramos desc tenemos que añadir desc
-  mutate(P12_7=case_when(P12_7=="2"~"1", P12_7=="3"~"3",TRUE~"1.1")) %>% 
-  count(P12_7) %>% 
-  mutate(n = as.double(n),) 
-  
-V_n2
-
-names(V_n2)
-attach(V_n2)
-str(P12_12)
-str(P12_7)
-plot(P12_12,P12_7)
-cor.test(P12_12,P12_7)
-
-#P value es 0.07171 y es menor a 0.1
-####
-###
-###
-####Correlación Buena, PERSONAS ENCUESTADAS QUE FUERON INSULTADAS EN LA NIÑEZ VS, LAS MISMAS PERSONAS ENCUESTADAS QUE INSULTAN A SUS HIJOS
-
-
-
-V_n <- TB_SEC_XII[!(TB_SEC_XII$P12_12 == "4"),]
-
-V_n2<-V_n %>%
-  filter(NOM_ENT == "CIUDAD DE MÉXICO",NOM_MUN=="GUSTAVO A. MADERO") %>%
-  group_by(NOM_MUN) %>% #Ordena de manera ascendente, si quisiéramos desc tenemos que añadir desc
-  mutate(P12_12=case_when(P12_2=="2"~"1", P12_12=="3"~"3",TRUE~"1.1")) %>% 
-  count(P12_12) %>% 
-  mutate(n2 = as.double(n),) 
-  
-
-V_n2
-V_n <- TB_SEC_XII[!(TB_SEC_XII$P12_12 == "4"),]
-
-V_n1<-V_n %>%
-  filter(NOM_ENT == "CIUDAD DE MÉXICO",NOM_MUN=="GUSTAVO A. MADERO") %>%
-  group_by(NOM_MUN) %>% #Ordena de manera ascendente, si quisiéramos desc tenemos que añadir desc
-  mutate(P12_7=case_when(P12_7=="2"~"1", P12_7=="3"~"3",TRUE~"1.1")) %>% 
-  count(P12_7) %>% 
-  mutate(n1 = as.double(n),)
-  
-V_n1
-
-#names(V_n2)
-#attach(V_n2)
-str(V_n1$n1)
-str(V_n2$n2)
-plot(V_n2$n2,V_n1$n1)
-cor.test(V_n2$n2,V_n1$n1)
-
-#P value es 0.06576 y es menor a 0.1
